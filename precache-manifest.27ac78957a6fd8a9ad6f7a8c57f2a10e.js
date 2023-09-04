3b4b773b65f3b2fb642c37ac3fa036f6a6acee06
Jeffrey Wongo <wongojeffrey@gmail.com>
2023-09-03 23:05:02 -0400

    Updates

---
+++ precache-manifest.27ac78957a6fd8a9ad6f7a8c57f2a10e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "62be56d2d8c3c20e0fa6",
+    "url": "/static/css/main.3672c835.chunk.css"
+  },
+  {
+    "revision": "62be56d2d8c3c20e0fa6",
+    "url": "/static/js/main.62be56d2.chunk.js"
+  },
+  {
+    "revision": "1118aa8e1d6091459212",
+    "url": "/static/js/1.1118aa8e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e362f2e0ec6b9621cfbfd69993882a42",
+    "url": "/index.html"
+  }
+];
